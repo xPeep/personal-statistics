e4dba84116f696f6d2b5ef951c85698e7bc5944b
xPeep <44339484+xPeep@users.noreply.github.com>
2021-05-16 02:45:53 +0200

    implement save jwt token, base login function

---
+++ src/Login/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const login = () => {
+  return (
+    <div>
+      <p>Username</p>
+      <input type="text" />
+      <p>Password</p>
+      <input type="password" />
+      <div>
+        <button>Login</button>
+        <button>Register</button>
+      </div>
+    </div>
+  );
+};
+
+export default login;
+// 
