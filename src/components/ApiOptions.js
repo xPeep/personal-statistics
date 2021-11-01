da53c5450f78ddc899c1b9d92f924df78a4c4096
Josef Bohm <josef.bohm@mainware.com>
2021-11-01 07:55:01 +0100

    add ver

--- src/components/ApiOptions.js
+++ src/components/ApiOptions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const apiUrl = "https://personal-statistics-server.herokuapp.com";
-const apiUrl = "http://localhost:8080";
+const apiUrl = "https://nnpro-backend.herokuapp.com/";
+//const apiUrl = "http://localhost:8080";
 
 axios.interceptors.request.use(
     (config) => {
